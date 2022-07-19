514729bc199ce3d9d1d08258b1286edfaccd8cc4
Gabriel de La Porta <gabrieldelaporta1@gmail.com>
2022-07-19 17:27:04 -0300

    Create product cards

---
+++ src/components/product-card.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
+import { CardActions } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  link: {
+    textDecoration: "none",
+    transitionProperty: "transform",
+    transitionDelay: "1.55s",
+    "&:hover, &:focus": {
+      transform: "scale(1.1)",
+    },
+  },
+  root: {
+    width: "20%",
+    height: "350px",
+    float: "left",
+    margin: "10px"
+  },
+  media: {
+    height: "200px",
+    width: "100%"
+  }
+});
+
+export default function ProductCard(props) {
+  const classes = useStyles();
+
+  return (
+    <Link to="" className={classes.link}>
+      <Card className={classes.root}>
+        <CardMedia
+          className={classes.media}
+          image={props.imageUrl}
+          title={props.title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          R$ {props.price}
+        </CardActions>
+      </Card>
+    </Link>
+  );
+}
