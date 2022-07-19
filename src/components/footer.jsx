2fcff5d97294c7c5a405a9634615cba894606ff8
Gabriel de La Porta <gabrieldelaporta1@gmail.com>
2022-07-19 17:26:49 -0300

    Create footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,68 @@
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+
+const Footer = () => {
+  const isLogged = !!localStorage.getItem('app-token');
+  return (
+    <footer>
+      <Box
+        px={{ xs: 3, sm: 10 }}
+        py={{ xs: 5, sm: 10 }}
+      >
+        <Container maxWidth="xl">
+          <Grid container spacing={5} className="footer">
+            <Grid item xs={12} sm={4}>
+              <Box borderBottom={1}>Help</Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Contact
+                </Link>
+              </Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Support
+                </Link>
+              </Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Privacy
+                </Link>
+              </Box>
+            </Grid>
+            {!isLogged && <Grid item xs={12} sm={4}>
+              <Box borderBottom={1}>Account</Box>
+              <Box>
+                <Link href="/login" color="inherit">Login</Link>
+              </Box>
+            </Grid>}
+            <Grid item xs={12} sm={4}>
+              <Box borderBottom={1}>Redes Sociais</Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Instagram
+                </Link>
+              </Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Facebook
+                </Link>
+              </Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  WhatsApp
+                </Link>
+              </Box>
+            </Grid>
+          </Grid>
+          <Box textAlign="center" pt={{ xs: 5, sm: 10 }} pb={{ xs: 5, sm: 0 }} className="footer">
+            Bia Pereira Semijoias &reg; {new Date().getFullYear()}
+          </Box>
+        </Container>
+      </Box>
+    </footer>
+  );
+}
+
+export default Footer;
